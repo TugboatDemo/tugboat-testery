06aeca546713402fa9e7c4dcd1bbd7af47f55070
Chris Harbert <chris.harbert@testery.io>
2020-06-17 17:46:16 -0500

    Testing build.

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,6 +2,12 @@ class HomePage {
 
     get searchToggle() { return $(".search-toggle"); }
     get searchBox() { return $("#search-form-1"); }
+
+    search(query) {
+        browser.execute("arguments[0].click();", this.searchToggle);
+        this.searchBox.setValue(query + "\n");
+    }
+
 }
 
 export default new HomePage()
